44c59919a49e0634c91e7802568eb17c70afe0e6
ahelloprojectfan <63974154+ahelloprojectfan@users.noreply.github.com>
2020-07-23 16:37:14 -0500

    Update 2020-03-03.js

--- src/js/data/2020-03-03.js
+++ src/js/data/2020-03-03.js
@@ -97,6 +97,849 @@ dataSet[dataSetVersion].characterData = [
             subkeyaki: ["ex-animal"]
         }
     }
+       ,
+    {
+        "name": ":Yanamin:",
+        "img": "https://i.imgur.com/8RIuQRo.png"
+        , opts: {
+            subkeyaki: ["emt-jj", "emt-cg"]
+        }
+    }
+    ,
+    {
+        "name": ":ChiiThink:",
+        "img": "https://i.imgur.com/Dzg1obv.png"
+        , opts: {
+            subkeyaki: ["emt-mm", "emt-cg"]
+        }
+    }
+    ,
+    {
+        "name": ":FunakiOops:",
+        "img": "https://i.imgur.com/eHXeLQ0.png"
+        , opts: {
+            subkeyaki: ["emt-anger", "emt-cg"]
+        }
+    }
+    ,
+    {
+        "name": ":YamakiShook:",
+        "img": "https://i.imgur.com/PerQNzB.png"
+        , opts: {
+            subkeyaki: ["emt-cg"]
+        }
+    }
+    ,
+    {
+        "name": ":ChiiHappy:",
+        "img": "https://i.imgur.com/yyO37Qt.png"
+        , opts: {
+            subkeyaki: ["emt-mm", "emt-cg"]
+        }
+    }
+    ,
+    {
+        "name": ":FunakiPOG:",
+        "img": "https://i.imgur.com/Yv7QF6F.png"
+        , opts: {
+            subkeyaki: ["emt-anger", "emt-cg"]
+        }
+    }
+    ,
+    {
+        "name": ":KikiDafuq:",
+        "img": "https://i.imgur.com/65hrML0.png"
+        , opts: {
+            subkeyaki: ["emt-tsu"]
+        }
+    }
+    ,
+    {
+        "name": ":AngryMao:",
+        "img": "https://i.imgur.com/4rZDeoz.png"
+        , opts: {
+            subkeyaki: ["emt-tsu"]
+        }
+    }
+    ,
+    {
+        "name": ":Ozekipray:",
+        "img": "https://i.imgur.com/rHtBf7F.png"
+        , opts: {
+            subkeyaki: ["emt-cg"]
+        }
+    }
+    ,
+    {
+        "name": ":SaorinCry:",
+        "img": "https://i.imgur.com/DpvLx4U.png"
+        , opts: {
+            subkeyaki: ["emt-tsu"]
+        }
+    }
+    ,
+    {
+        "name": ":RikoSmug:",
+        "img": "https://i.imgur.com/DirjrVo.png"
+        , opts: {
+            subkeyaki: ["emt-tsu"]
+        }
+    }
+    ,
+    {
+        "name": ":AmiOkay:",
+        "img": "https://i.imgur.com/a8Y4WMF.png"
+        , opts: {
+            subkeyaki: ["emt-tsu"]
+        }
+    }
+    ,
+    {
+        "name": ":IchiokaPoint: ",
+        "img": "https://i.imgur.com/DXlBIXZ.png"
+        , opts: {
+            subkeyaki: ["emt-by"]
+        }
+    }
+    ,
+    {
+        "name": ":omizupeek:",
+        "img": "https://i.imgur.com/qZxXYmC.png"
+        , opts: {
+            subkeyaki: ["emt-tsu"]
+        }
+    }
+    ,
+    {
+        "name": ":RisaLewd:",
+        "img": "https://i.imgur.com/a9muCK0.png"
+        , opts: {
+            subkeyaki: ["emt-tsu"]
+        }
+    }
+    ,
+    {
+        "name": ":HPFansLogo:",
+        "img": "https://i.imgur.com/rHcH4iR.png"
+        , opts: {
+            subkeyaki: ["emt-special"]
+        }
+    }
+    ,
+    {
+        "name": ":KishimonLaugh:",
+        "img": "https://i.imgur.com/8d1p4eq.png"
+        , opts: {
+            subkeyaki: ["emt-tsu"]
+        }
+    }
+    ,
+    {
+        "name": ":HonokaPray:",
+        "img": "https://i.imgur.com/m3rEeu5.png"
+        , opts: {
+            subkeyaki: ["emt-by"]
+        }
+    }
+    ,
+    {
+        "name": ":MiimiThink:",
+        "img": "https://i.imgur.com/lcarFgB.png"
+        , opts: {
+            subkeyaki: ["emt-by"]
+        }
+    }
+    ,
+    {
+        "name": ":OotaAmazement:",
+        "img": "https://i.imgur.com/08XTZxl.png"
+        , opts: {
+            subkeyaki: ["emt-anger"]
+        }
+    }
+    ,
+    {
+        "name": ":OkamurderSan:",
+        "img": "https://i.imgur.com/SNbhnrj.png"
+        , opts: {
+            subkeyaki: ["emt-by"]
+        }
+    }
+    ,
+    {
+        "name": ":Yuhaneshy:",
+        "img": "https://i.imgur.com/Nl42j7r.png"
+        , opts: {
+            subkeyaki: ["emt-by"]
+        }
+    }
+    ,
+    {
+        "name": ":KassaHappy:",
+        "img": "https://i.imgur.com/d7Ag9aU.png"
+        , opts: {
+            subkeyaki: ["emt-anger"]
+        }
+    }
+    ,
+    {
+        "name": ":RikakoStare:",
+        "img": "https://i.imgur.com/ebeGi29.png"
+        , opts: {
+            subkeyaki: ["emt-anger"]
+        }
+    }
+    ,
+    {
+        "name": ":KisoraCop:",
+        "img": "https://i.imgur.com/M9D4xwZ.png"
+        , opts: {
+            subkeyaki: ["emt-tsu"]
+        }
+    }
+    ,
+    {
+        "name": ":Kaedii:",
+        "img": "https://i.imgur.com/wySEton.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+    ,
+    {
+        "name": ":KawamuuShook:",
+        "img": "https://i.imgur.com/dCBgyDG.png"
+        , opts: {
+            subkeyaki: ["emt-anger"]
+        }
+    }
+    ,
+    {
+        "name": ":SayuUSA:",
+        "img": "https://i.imgur.com/B0rQDa5.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+    ,
+    {
+        "name": ":KamikoMad:",
+        "img": "https://i.imgur.com/CtlFyGf.png"
+        , opts: {
+            subkeyaki: ["emt-anger"]
+        }
+    }
+    ,
+    {
+        "name": ":ShioriThink:",
+        "img": "https://i.imgur.com/ZRBm2A8.png"
+        , opts: {
+            subkeyaki: ["emt-by"]
+        }
+    }
+    ,
+    {
+        "name": ":TakeWhat: ",
+        "img": "https://i.imgur.com/O6ZeWWu.png"
+        , opts: {
+            subkeyaki: ["emt-anger"]
+        }
+    }
+    ,
+    {
+        "name": ":MiimiHeh:",
+        "img": "https://i.imgur.com/b6HBAXt.png"
+        , opts: {
+            subkeyaki: ["emt-by"]
+        }
+    }
+    ,
+    {
+        "name": ":RihoThumbs:",
+        "img": "https://i.imgur.com/aUjTBQy.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+    ,
+    {
+        "name": ":Maablush:",
+        "img": "https://i.imgur.com/2EEeAbj.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+    ,
+    {
+        "name": ":RikaAnnoyed:",
+        "img": "https://i.imgur.com/Lf7ycei.png"
+        , opts: {
+            subkeyaki: ["emt-by"]
+        }
+    }
+    ,
+    {
+        "name": ":chii: ",
+        "img": "https://i.imgur.com/O7BCJ9F.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+    ,
+    {
+        "name": ":KaediiCool:",
+        "img": "https://i.imgur.com/sAZYTeB.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+    ,
+    {
+        "name": ":Reinya:",
+        "img": "https://i.imgur.com/8lme3v2.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+    ,
+    {
+        "name": ":SatoyoshiGanbatte:",
+        "img": "https://i.imgur.com/UnWsxFB.png"
+        , opts: {
+            subkeyaki: ["emt-by"]
+        }
+    }
+    ,
+    {
+        "name": ":DiiThumbs:",
+        "img": "https://i.imgur.com/olqoDYw.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+    ,
+    {
+        "name": ":RihoBlind:",
+        "img": "https://i.imgur.com/gYmQYEo.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+    ,
+    {
+        "name": ":OdaLook:",
+        "img": "https://i.imgur.com/OZOB6bq.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+    ,
+    {
+        "name": ":ShiorinCrazy:",
+        "img": "https://i.imgur.com/agMnQJl.png"
+        , opts: {
+            subkeyaki: ["emt-by"]
+        }
+    }
+    ,
+    {
+        "name": ":Yanacool:",
+        "img": "https://i.imgur.com/i8RzPGB.png"
+        , opts: {
+            subkeyaki: ["emt-cg", "emt-jj"]
+        }
+    }
+    ,
+    {
+        "name": ":AyumiMad:",
+        "img": "https://i.imgur.com/yjgGWA8.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+    ,
+    {
+        "name": ":RioLuL:",
+        "img": "https://i.imgur.com/20OtNC7.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+    ,
+    {
+        "name": ":Ayumilul:",
+        "img": "https://i.imgur.com/vS25WFR.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+    ,
+    {
+        "name": ":MuroSarcasm:",
+        "img": "https://i.imgur.com/vxyaSZJ.png"
+        , opts: {
+            subkeyaki: ["emt-anger"]
+        }
+    }
+    ,
+    {
+        "name": ":AyumiPeek:",
+        "img": "https://i.imgur.com/wow45Kz.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+    ,
+    {
+        "name": ":ReiReiAngry:",
+        "img": "https://i.imgur.com/QYKlXNn.png"
+        , opts: {
+            subkeyaki: ["emt-jj"]
+        }
+    }
+    ,
+    {
+        "name": ":RuruTea:",
+        "img": "https://i.imgur.com/oPpwBiT.png"
+        , opts: {
+            subkeyaki: ["emt-jj"]
+        }
+    }
+    ,
+    {
+        "name": ":Sayubee:",
+        "img": "https://i.imgur.com/5HVu2b6.png"
+        , opts: {
+            subkeyaki: ["emt-jj"]
+        }
+    }
+    ,
+    {
+        "name": ":RikakoDisgust:",
+        "img": "https://i.imgur.com/tNAA8Bc.png"
+        , opts: {
+            subkeyaki: ["emt-anger"]
+        }
+    }
+    ,
+    {
+        "name": ":SayuBeePOG:",
+        "img": "https://i.imgur.com/8A6VWGy.png"
+        , opts: {
+            subkeyaki: ["emt-jj"]
+        }
+    }
+    ,
+    {
+        "name": ":RikaGun:",
+        "img": "https://i.imgur.com/lq1yaT5.png"
+        , opts: {
+            subkeyaki: ["emt-by"]
+        }
+    }
+    ,
+    {
+        "name": ":Homasad:",
+        "img": "https://i.imgur.com/DB7az5f.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+
+    ,
+    {
+        "name": ":ChiiShrug:",
+        "img": "https://i.imgur.com/NBlCkh5.png"
+        , opts: {
+            subkeyaki: ["emt-mm", "emt-cg"]
+        }
+    }
+
+    ,
+    {
+        "name": ":sayaparty:",
+        "img": "https://i.imgur.com/tDp3sbi.png"
+        , opts: {
+            subkeyaki: ["emt-by"]
+        }
+    }
+
+    ,
+    {
+        "name": ":RiaiThreaten: ",
+        "img": "https://i.imgur.com/3GBis5v.png"
+        , opts: {
+            subkeyaki: ["emt-jj"]
+        }
+    }
+
+    ,
+    {
+        "name": ":Reinacop:",
+        "img": "https://i.imgur.com/IosE7lP.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+
+    ,
+    {
+        "name": ":TomokoPeek:",
+        "img": "https://i.imgur.com/PVhTKuU.png"
+        , opts: {
+            subkeyaki: ["emt-jj"]
+        }
+    }
+
+    ,
+    {
+        "name": ":ManakaScrem:",
+        "img": "https://i.imgur.com/A5mdSaf.png"
+        , opts: {
+            subkeyaki: ["emt-jj"]
+        }
+    }
+
+    ,
+    {
+        "name": ":KuruminShocked:",
+        "img": "https://i.imgur.com/dkdJgd5.png"
+        , opts: {
+            subkeyaki: ["emt-by"]
+        }
+    }
+
+    ,
+    {
+        "name": ":MiyoSleep:",
+        "img": "https://i.imgur.com/PhrFtX1.png"
+        , opts: {
+            subkeyaki: ["emt-by"]
+        }
+    }
+
+    ,
+    {
+        "name": ":OgataOkay: ",
+        "img": "https://i.imgur.com/cpoHX0X.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+
+    ,
+    {
+        "name": ":KokoroSmart: ",
+        "img": "https://i.imgur.com/awRDY8d.png"
+        , opts: {
+            subkeyaki: ["emt-by"]
+        }
+    }
+
+    ,
+    {
+        "name": ":HarunaHush:",
+        "img": "https://i.imgur.com/Zr7TsDW.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+
+    ,
+    {
+        "name": ":MikiSalad1:",
+        "img": "https://i.imgur.com/QqabCUs.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+
+    ,
+    {
+        "name": ":Fukustop:",
+        "img": "https://i.imgur.com/K7BmCnG.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+
+    ,
+    {
+        "name": ":RinSmile:",
+        "img": "https://i.imgur.com/mzFxlmF.png"
+        , opts: {
+            subkeyaki: ["emt-anger"]
+        }
+    }
+
+    ,
+    {
+        "name": ":Icoolta:",
+        "img": "https://i.imgur.com/uagh65p.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+
+    ,
+    {
+        "name": ":ManakanPray:",
+        "img": "https://i.imgur.com/hxpZ9oE.png"
+        , opts: {
+            subkeyaki: ["emt-jj"]
+        }
+    }
+
+    ,
+    {
+        "name": ":MuuReally:",
+        "img": "https://i.imgur.com/8w51fqI.png"
+        , opts: {
+            subkeyaki: ["emt-jj"]
+        }
+    }
+
+    ,
+    {
+        "name": ":Haachin:",
+        "img": "https://i.imgur.com/4NfUQHW.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+
+    ,
+    {
+        "name": ":RioSaysYes:",
+        "img": "https://i.imgur.com/J1cTcxL.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+
+    ,
+    {
+        "name": ":IchiokaThink:",
+        "img": "https://i.imgur.com/0FpejbH.png"
+        , opts: {
+            subkeyaki: ["emt-by"]
+        }
+    }
+
+    ,
+    {
+        "name": ":KarinGanbatte:",
+        "img": "https://i.imgur.com/1Zvg9fo.png"
+        , opts: {
+            subkeyaki: ["emt-jj"]
+        }
+    }
+
+    ,
+    {
+        "name": ":RioBruhWords:",
+        "img": "https://i.imgur.com/K6btx4X.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+
+    ,
+    {
+        "name": ":Maatroll:",
+        "img": "https://i.imgur.com/U8j3lU2.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+
+    ,
+    {
+        "name": ":RuruOhYou:",
+        "img": "https://i.imgur.com/MRtEcuz.png"
+        , opts: {
+            subkeyaki: ["emt-jj"]
+        }
+    }
+
+    ,
+    {
+        "name": ":DisappointedLayla:",
+        "img": "https://i.imgur.com/7bXPKeG.png"
+        , opts: {
+            subkeyaki: ["emt-anger"]
+        }
+    }
+    ,
+    {
+        "name": ":YumeSmile:",
+        "img": "https://i.imgur.com/IEsszxD.png"
+        , opts: {
+            subkeyaki: ["emt-jj"]
+        }
+    }
+    ,
+    {
+        "name": ":Maria:",
+        "img": "https://i.imgur.com/cuirOW6.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+
+    ,
+    {
+        "name": ":MikiSalad2:",
+        "img": "https://i.imgur.com/Ak2J7qx.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+
+    ,
+    {
+        "name": ":Tsunku: ",
+        "img": "https://i.imgur.com/WFJvEJp.png"
+        , opts: {
+            subkeyaki: ["emt-special"]
+        }
+    }
+
+    ,
+    {
+        "name": ":MikiOmg:",
+        "img": "https://i.imgur.com/Xy2FZug.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+
+    ,
+    {
+        "name": ":MiimiSTOP:",
+        "img": "https://i.imgur.com/P077reO.png"
+        , opts: {
+            subkeyaki: ["emt-by"]
+        }
+    }
+
+    ,
+    {
+        "name": ":RinNotLikeThis:",
+        "img": "https://i.imgur.com/WiwF6A8.png"
+        , opts: {
+            subkeyaki: ["emt-anger"]
+        }
+    }
+
+    ,
+    {
+        "name": ":HomarePeace:",
+        "img": "https://i.imgur.com/wikU3Oe.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+
+    ,
+    {
+        "name": ":SayarinThink: ",
+        "img": "https://i.imgur.com/34S3SnY.png"
+        , opts: {
+            subkeyaki: ["emt-by"]
+        }
+    }
+
+    ,
+    {
+        "name": ":MomohimeSad:",
+        "img": "https://i.imgur.com/zKYWZL9.png"
+        , opts: {
+            subkeyaki: ["emt-by"]
+        }
+    }
+
+    ,
+    {
+        "name": ":AkaneCat:",
+        "img": "https://i.imgur.com/N6cPpDW.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+
+    ,
+    {
+        "name": ":MiimiHappy:",
+        "img": "https://i.imgur.com/64TvmRE.png"
+        , opts: {
+            subkeyaki: ["emt-by"]
+        }
+    }
+
+    ,
+    {
+        "name": ":MeiPeace:",
+        "img": "https://i.imgur.com/p0H5lDe.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+
+    ,
+    {
+        "name": ":MaaHeadache:",
+        "img": "https://i.imgur.com/bczMGtJ.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+
+    ,
+    {
+        "name": ":YumePigeon:",
+        "img": "https://i.imgur.com/2pa16YW.png"
+        , opts: {
+            subkeyaki: ["emt-jj"]
+        }
+    }
+
+    ,
+    {
+        "name": ":RioSaysNo:",
+        "img": "https://i.imgur.com/JuXZKcB.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+
+    ,
+    {
+        "name": ":ChiiDiscomfort:",
+        "img": "https://i.imgur.com/8MwTEUD.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+
+    ,
+    {
+        "name": ":RihoMetal:",
+        "img": "https://i.imgur.com/Qwtqs7m.png"
+        , opts: {
+            subkeyaki: ["emt-mm"]
+        }
+    }
+
+    ,
+    {
+        "name": ":Karinwince:",
+        "img": "https://i.imgur.com/HOXGVEN.png"
+        , opts: {
+            subkeyaki: ["emt-jj"]
+        }
+    }
+
 
 ];
 
@@ -107,9 +950,14 @@ dataSet[dataSetVersion].options = [
         tooltip: 'Check this to restrict to certain group',
         checked: true,
         sub: [
-      
             { name: 'H!P pets and other animals', key: 'animal' },
-            { name: 'Ex-H!P pets and other animals', key: 'ex-animal' }
+            { name: 'Ex-H!P pets and other animals', key: 'ex-animal' },
+            { name: 'Emotes Morning Musume 20', key: 'emt-mm' },
+            { name: 'Emotes Juice=Juice', key: 'emt-jj' },
+            { name: 'Emotes Angerme', key: 'emt-anger' },
+            { name: 'Emotes Tsubaki Factory', key: 'emt-tsu' },
+            { name: 'Emotes BEYOOOOONDS', key: 'emt-by' },
+            { name: 'Emotes Ex-Country Girls', key: 'emt-cg' },
         ]
     }
 
