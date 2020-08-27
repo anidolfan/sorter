6d40742f20ab941288f74c041a46113d33d60bda
ahelloprojectfan <63974154+ahelloprojectfan@users.noreply.github.com>
2020-08-27 16:26:38 -0500

    Add files via upload

--- src/js/data/2020-03-03.js
+++ src/js/data/2020-03-03.js
@@ -938,7 +938,563 @@ dataSet[dataSetVersion].characterData = [
         , opts: {
             subkeyaki: ["emt-jj"]
         }
+    },
+
+
+    //singles 
+    {
+        "name": "Ai no Tane (1997)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/f/f2/AinoTane-r.jpg/revision/latest?cb=20100307201226"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Morning Coffee (1998)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/0/0a/MorningCoffee-r.jpg/revision/latest?cb=20150121022910"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Summer Night Town (1998)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/a/af/SummerNightTown-rr.jpg/revision/latest?cb=20150121025142"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Daite Hold on Me! (1998)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/6/66/DaiteHOLDONME-rr.jpg/revision/latest?cb=20150121093557"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Memory Seishun no Hikari (1999) ",
+        "img": "https://i.imgur.com/dJoFaQT.jpg"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Manatsu no Kōsen (1999)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/3/33/ManatsunoKousen-rr.jpg/revision/latest?cb=20100225001029"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Furusato (1999)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/4/43/Furusato-rr.jpg/revision/latest?cb=20100225001259"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Love Machine (1999)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/4/4b/LOVEMachine-r.jpg/revision/latest?cb=20091221003954"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Koi no Dance Site (2000)",
+        "img": "https://i.imgur.com/0rJSNZU.jpg"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Happy Summer Wedding (2000)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/0/0d/HappySummerWedding-r.jpg/revision/latest?cb=20100225002147"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "I Wish (2000)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/7/75/IWISH-r.jpg/revision/latest?cb=20091221003423"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Ren'ai Revolution 21 (2000)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/9/9b/RenaiRevolution21-lp.jpg/revision/latest?cb=20100305015702"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "The Peace! (2001)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/3/3e/ThePeace-r.jpg/revision/latest?cb=20100225003355"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Mr. Moonlight (Ai no Big Band) (2001) ",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/4/47/MrMoonlightAinoBigBand-r.jpg/revision/latest?cb=20091126042002"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Sōda! We're Alive (2002)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/6/66/SoudaWereALIVE-r.jpg/revision/latest?cb=20100225004118"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Do It! Now (2002)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/9/99/DoitNow-r.jpg/revision/latest/scale-to-width-down/699?cb=20150628075959"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Koko ni Iruzee! (2002)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/a/af/KokoniIruzee-r.jpg/revision/latest?cb=20100305023354"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Morning Musume no Hyokkori Hyōtanjima (2003) ",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/e/e5/MorningMusumenoHyokkoriHyoutanjima-r.jpg/revision/latest?cb=20100305023847"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "As for One Day (2003)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/d/d3/ASFORONEDAY-r.jpg/revision/latest?cb=20100305024806"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Shabondama (2003)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/5/59/Shabondama-r.jpg/revision/latest?cb=20150416193402"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Go Girl (Koi no Victory) (2003) ",
+        "img": "https://i.imgur.com/nDdZmAR.jpg"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Ai Araba It's All Right (2004)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/8/87/AiArabaITSALLRIGHT-r.jpg/revision/latest?cb=20100305220948"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Roman (My Dear Boy) (2004)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/e/e4/RomanMYDEARBOY-r.jpg/revision/latest?cb=20100305221734"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Joshi Kashimashi Monogatari (2004) ",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/7/77/JoshiKashimashiMonogatari-r.jpg/revision/latest?cb=20140323100151"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Namida ga Tomaranai Hōkago (2004)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/a/a1/NamidagaTomaranaiHoukago-r.jpg/revision/latest?cb=20100305223135"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "The Manpower!!! (2005)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/d/dc/THEManpower-r.jpg/revision/latest?cb=20100305022053"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Osaka Koi no Uta (2005)",
+        "img": "https://i.imgur.com/9iDQrm8.jpg"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Iroppoi Jirettai (2005)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/3/31/IroppoiJirettai-r.jpg/revision/latest?cb=20100305020721"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Chokkan 2 (Nogashita Sakana wa Ōkiizo!) (2005) ",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/b/b2/Chokkan2NogashitaSakanawaOokiizo-r.jpg/revision/latest?cb=20091221001601"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Sexy Boy (Soyokaze ni Yorisotte) (2006)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/b/bf/SEXYBOYSoyokazeniYorisotte-r.jpg/revision/latest?cb=20150527141548"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Ambitious! Yashinteki de Ii Jan (2006)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/c/cb/AmbitiousYashintekideIijan-r.jpg/revision/latest?cb=20091012103526"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Aruiteru (2006)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/c/cc/Aruiteru-r.jpg/revision/latest?cb=20130202170044"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Egao Yes Nude (2007)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/c/c5/EgaoYESNude-r.jpg/revision/latest?cb=20091012100517"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Kanashimi Twilight (2007)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/9/98/KanashimiTwilight-la.jpg/revision/latest?cb=20091011183934"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Onna ni Sachi Are (2007)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/a/ab/OnnaniSachiAre-r.jpg/revision/latest/scale-to-width-down/700?cb=20150628081726"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Mikan (2007)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/9/9b/Mikan-r.jpg/revision/latest/scale-to-width-down/700?cb=20091011170036"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Resonant Blue (2008)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/5/5b/ResonantBlue-r.jpg/revision/latest/scale-to-width-down/700?cb=20091011134010"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Pepper Keibu (2008)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/a/ad/PepperKeibu-r.jpg/revision/latest?cb=20130205004440"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Naichau Kamo (2009)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/d/d5/NaichauKamo-r.jpg/revision/latest?cb=20091011141515"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Shōganai Yume Oibito (2009)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/0/05/ShouganaiYumeOibito-r.jpg/revision/latest?cb=20091011144928"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Nanchatte Ren'ai (2009)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/b/b1/NanchatteRenai-r.jpg/revision/latest/scale-to-width-down/699?cb=20090928124945"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Kimagure Princess (2009)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/f/f2/KimagurePrincess-r.jpg/revision/latest?cb=20120908141600"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Onna ga Medatte Naze Ikenai (2010)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/b/b1/OnnagaMedatteNazeIkenai-r.jpg/revision/latest/scale-to-width-down/700?cb=20200822090621"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Seishun Collection (2010)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/6/69/SeishunCollection-r.jpg/revision/latest?cb=20150421205638"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Onna to Otoko no Lullaby Game (2010)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/d/db/OnnatoOtokonoLullabyGame-r.jpg/revision/latest/scale-to-width-down/699?cb=20200822090827"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Maji Desu ka Ska! (2011)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/f/f9/MajiDesukaSka-r.jpg/revision/latest?cb=20110310034826"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Only You (2011)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/e/e7/Onlyyou-la.jpg/revision/latest?cb=20110520165648"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Kono Chikyū no Heiwa o Honki de Negatterun Da yo! / Kare to Issho ni Omise ga Shitai! (2011)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/a/ab/KonoChikyuunoHeiwawoHonkideNegatterundayo-r.jpg/revision/latest?cb=20111210151457"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Pyocopyoco Ultra (2012) ",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/2/2a/PyocopyocoUltra-r.jpg/revision/latest/scale-to-width-down/699?cb=20200607140944"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Ren'ai Hunter (2012)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/c/c5/RenaiHunter-r.jpg/revision/latest?cb=20200607135857"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "One Two Three / The Matenrō Show (2012) ",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/c/c5/OneTwoThree-r.jpg/revision/latest?cb=20200607134425"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Wakuteka Take a Chance (2012)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/e/e4/WakutekaTakeachance-r.jpg/revision/latest/scale-to-width-down/700?cb=20200607133331"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Help Me!! (2013)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/b/b6/Helpme-rb.jpg/revision/latest/scale-to-width-down/699?cb=20200607095017"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Brainstorming / Kimi Sae Ireba Nani mo Iranai (2013)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/0/06/Brainstorming-rb.jpg/revision/latest/scale-to-width-down/700?cb=20200607093928"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Wagamama Ki no Mama Ai no Joke / Ai no Gundan (2013)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/0/04/WagamamaKinoMamaAinoJoke-rb.jpg/revision/latest/scale-to-width-down/700?cb=20200607083828"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Egao no Kimi wa Taiyō sa / Kimi no Kawari wa Iyashinai / What is Love? (2014)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/d/da/EgaonoKimiwaTaiyousa-rb.jpg/revision/latest?cb=20150428165330"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Toki o Koe Sora o Koe / Password is 0 (2014)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/7/79/TokiwoKoeSorawoKoe-r.jpg/revision/latest/scale-to-width-down/700?cb=20140320145701"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Tiki Bun / Shabadaba Dū / Mikaeri Bijin (2014)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/5/56/TIKIBUN-r.jpg/revision/latest/scale-to-width-down/700?cb=20200605084226"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Seishun Kozo ga Naiteiru / Yūgure wa Ameagari / Ima Koko Kara[32] (2015)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/b/ba/SeishunKozougaNaiteiru-r.jpg/revision/latest/scale-to-width-down/699?cb=20200605084044"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Oh My Wish! / Sukatto My Heart / Ima Sugu Tobikomu Yūki[34] (2015)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/5/5b/Ohmywish-rb.jpg/revision/latest?cb=20150727064254"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Tsumetai Kaze to Kataomoi / Endless Sky / One and Only[36] (2015)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/9/93/TsumetaiKazetoKataomoi-rb.jpg/revision/latest/scale-to-width-down/700?cb=20151210065029"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Utakata Saturday Night! / The Vision / Tokyo to Iu Katasumi[38] (2016)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/d/d8/UtakataSaturdayNight-r.jpg/revision/latest/scale-to-width-down/699?cb=20160416084942"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Sexy Cat no Enzetsu / Mukidashi de Mukiatte / Sō ja Nai[39] (2016)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/3/3a/SexyCatnoEnzetsu-r.jpg/revision/latest/scale-to-width-down/700?cb=20200605083504"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Brand New Morning / Jealousy Jealousy[40] (2017)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/d/d6/BRANDNEWMORNING-r.jpg/revision/latest/scale-to-width-down/700?cb=20200605082908"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Jama Shinaide Here We Go! / Dokyū no Go Sign / Wakaindashi! (2017)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/4/42/JamaShinaideHereWeGo-r.jpg/revision/latest/scale-to-width-down/700?cb=20180201050356"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Are You Happy? / A Gonna (2018)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/5/5e/AreyouHappy-r.jpg/revision/latest/scale-to-width-down/700?cb=20200604190716"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Furari Ginza / Jiyū na Kuni Dakara (2018)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/4/4e/Furari_Ginza-r.jpg/revision/latest/scale-to-width-down/699?cb=20200604190520"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Jinsei Blues / Seishun Night (2019)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/6/6c/JinseiBlues-r.jpg/revision/latest/scale-to-width-down/699?cb=20190611100427"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
+    },
+    {
+        "name": "Kokoro & Karada / Lovepedia / Ningen Kankei No Way Way (2020)",
+        "img": "https://vignette.wikia.nocookie.net/helloproject/images/4/46/KOKORO%26KARADA-rb.jpg/revision/latest?cb=20191215071556"
+        , opts:{
+            subkeyaki: ["mmsingle"]
+        }
+    
     }
+    
 
 
 ];
@@ -950,6 +1506,7 @@ dataSet[dataSetVersion].options = [
         tooltip: 'Check this to restrict to certain group',
         checked: true,
         sub: [
+            { name: 'Morning Musume Singles', key: 'mmsingle' },
             { name: 'H!P pets and other animals', key: 'animal' },
             { name: 'Ex-H!P pets and other animals', key: 'ex-animal' },
             { name: 'Emotes Morning Musume 20', key: 'emt-mm' },
