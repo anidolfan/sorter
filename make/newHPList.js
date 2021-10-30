877d22548b1a4f4472bf62cba082b9172eac4833
ahelloprojectfan <63974154+ahelloprojectfan@users.noreply.github.com>
2021-10-30 06:26:46 -0500

    Add files via upload

---
+++ make/newHPList.js
@@ -0,0 +1,228 @@
+let newList = [
+  
+  //JJ
+  {
+    "name": "金澤朋子",
+    "name2": "Kanazawa Tomoko",
+    "img": "https://i.imgur.com/BnA1nYE.jpg"
+    ,
+    "group": [
+      "jj"
+    ]
+  },
+  {
+    "name": "植村あかり",
+    "name2": "Uemura Akari",
+    "img": "https://i.imgur.com/GEx9Taf.jpg"
+    ,
+    "group": [
+      "jj"
+    ]
+  },
+  {
+    "name": "稲場愛香",
+    "name2": "Inaba Manaka",
+    "img": "https://i.imgur.com/TBopG1W.jpg"
+    ,
+    "group": [
+      "jj",
+      "cg"
+    ]
+  },
+  {
+    "name": "井上玲音",
+    "name2": "Inoue Rei",
+    "img": "https://i.imgur.com/ooKnpVv.jpg"
+    ,
+    "group": [
+      "jj",
+      "kf"
+    ]
+  },
+  {
+    "name": "段原瑠々",
+    "name2": "Dambara Ruru",
+    "img": "https://i.imgur.com/DVVlpn7.jpg"
+    ,
+    "group": [
+      "jj"
+    ]
+  },
+  {
+    "name": "工藤由愛",
+    "name2": "Kudo Yume",
+    "img": "https://i.imgur.com/PiBevPh.jpg"
+    ,
+    "group": [
+      "jj"
+    ]
+  },
+  {
+    "name": "松永里愛",
+    "name2": "Matsunaga Riai",
+    "img": "https://i.imgur.com/t6NEMBZ.jpg"
+    ,
+    "group": [
+      "jj"
+    ]
+  },
+  {
+    "name": "江端妃咲",
+    "name2": "Ebata Kisaki",
+    "img": "https://i.imgur.com/B5tdsl2.jpg"
+    ,
+    "group": [
+      "jj"
+    ]
+  },
+  {
+    "name": "有澤一華",
+    "name2": "Arisawa Ichika",
+    "img": "https://i.imgur.com/O64kXfz.jpg"
+    ,
+    "group": [
+      "jj"
+    ]
+  },
+  {
+    "name": "入江里咲",
+    "name2": "Irie Risa",
+    "img": "https://i.imgur.com/wUSXk54.jpg"
+    ,
+    "group": [
+      "jj"
+    ]
+  },
+
+
+  //MM
+  {
+    "name": "譜久村聖",
+    "name2": "Fukumura Mizuki",
+    "img": "https://i.imgur.com/1zDygV1.jpg"
+    ,
+    "group": [
+      "mm"
+    ]
+  },
+  {
+    "name": "生田衣梨奈",
+    "name2": "Ikuta Erina",
+    "img": "https://i.imgur.com/irS3rD1.jpg",
+    "group": [
+      "mm"
+    ]
+  },
+  {
+    "name": "石田亜佑美",
+    "name2": "Ishida Ayumi",
+    "img": "https://i.imgur.com/p63HI51.jpg"
+    ,
+    "group": [
+      "mm"
+    ]
+  },
+  {
+    "name": "佐藤優樹",
+    "name2": "Sato Masaki",
+    "img": "https://i.imgur.com/CTeTH09.jpg"
+    ,
+    "group": [
+      "mm"
+    ]
+  },
+  {
+    "name": "小田さくら",
+    "name2": "Oda Sakura",
+    "img": "https://i.imgur.com/B5cHAG2.jpg"
+    ,
+    "group": [
+      "mm"
+    ]
+  },
+  {
+    "name": "野中美希",
+    "name2": "Nonaka Miki",
+    "img": "https://i.imgur.com/1sURzaD.jpg"
+    ,
+    "group": [
+      "mm"
+    ]
+  },
+  {
+    "name": "牧野真莉愛",
+    "name2": "Makino Maria",
+    "img": "https://i.imgur.com/PlZ1XDM.jpg"
+    ,
+    "group": [
+      "mm"
+    ]
+  },
+  {
+    "name": "羽賀朱音",
+    "name2": "Haga Akane",
+    "img": "https://i.imgur.com/vG80PkQ.jpg"
+    ,
+    "group": [
+      "mm"
+    ]
+  },
+  {
+    "name": "加賀楓",
+    "name2": "Kaga Kaede",
+    "img": "https://i.imgur.com/KQvP2F8.jpg"
+    ,
+    "group": [
+      "mm"
+    ]
+  },
+  {
+    "name": "横山玲奈",
+    "name2": "Yokoyama Reina",
+    "img": "https://i.imgur.com/I2aa7GC.jpg"
+    ,
+    "group": [
+      "mm"
+    ]
+  },
+  {
+    "name": "森戸知沙希",
+    "name2": "Morito Chisaki",
+    "img": "https://i.imgur.com/jRGFtAF.jpg"
+    ,
+    "group": [
+      "mm",
+      "cg"
+    ]
+  },
+  {
+    "name": "北川莉央",
+    "name2": "Kitagawa Rio",
+    "img": "https://i.imgur.com/CSA26uq.jpg"
+    ,
+    "group": [
+      "mm"
+    ]
+  },
+  {
+    "name": "岡村ほまれ",
+    "name2": "Okamura Homare",
+    "img": "https://i.imgur.com/kWwgvH4.jpg"
+    ,
+    "group": [
+      "mm"
+    ]
+  },
+  {
+    "name": "山﨑愛生",
+    "name2": "Yamazaki Mei",
+    "img": "https://i.imgur.com/yUoHPBF.jpg"
+    ,
+    "group": [
+      "mm"
+    ]
+  }
+
+
+]
+
